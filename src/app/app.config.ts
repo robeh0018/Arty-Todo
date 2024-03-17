f78393b605211c5945da4faa4bf98dc6ef475c46
robeh0018 <robeh0018@gmail.com>
2024-03-17 10:48:15 -1100

    SSR added and flowbite on tailwind.config.js  issue fixed.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,9 @@ import {ApplicationConfig} from '@angular/core';
 import {provideRouter, withViewTransitions} from '@angular/router';
 
 import {routes} from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {provideClientHydration} from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withViewTransitions()), provideAnimationsAsync()]
+  providers: [provideRouter(routes, withViewTransitions()), provideAnimationsAsync(), provideClientHydration()]
 };
